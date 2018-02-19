780e9f8ebbb80ef1765b5065ccdbee28a43dda69
Phil Czek <phil.czek@gmail.com>
2018-02-19 13:08:36 +0100

    adds an AddressDetailed Component, which chose the transacitons and balance of an account

--- App.js
+++ App.js
@@ -1,77 +1,83 @@
-import React, {Component} from 'react'
-import {StyleSheet, View, SafeAreaView} from 'react-native'
-import {connect} from 'react-redux'
+import React, { Component } from 'react';
+import { StyleSheet, View, SafeAreaView } from 'react-native';
+import { connect } from 'react-redux';
 import Swiper from 'react-native-swiper';
-import {Text, Button, TextInput, Heading, Row} from '@shoutem/ui'
+import { Text, Button, TextInput, Heading, Row } from '@shoutem/ui';
 
-import Scanner from './src/components/Scanner'
-import {Account} from './src/components/Account'
-import {addAndPopulateAccount} from './src/actions'
+import Scanner from './src/components/Scanner';
+import { Account } from './src/components/Account';
+import { AddressDetailed } from './src/components/AddressDetailed';
+import { addAndPopulateAccount } from './src/actions';
 
 class App extends Component {
-  state = {
-    cameraActive: false
-  }
+	state = {
+		cameraActive: false
+	};
 
-  onIndexChanged = (index) => {
-    this.setState({
-      cameraActive: index === 0
-    })
-  }
+	onIndexChanged = (index) => {
+		this.setState({
+			cameraActive: index === 0
+		});
+	};
 
-  _qrCodeScanned = (qrCode) => {
-    this
-      .props
-      .addAndPopulateAccount(qrCode.data)
+	_qrCodeScanned = (qrCode) => {
+		this.props.addAndPopulateAccount(qrCode.data);
 
-    this
-      .swiper
-      .scrollBy(1)
-  }
+		this.swiper.scrollBy(1);
+	};
 
-  renderAccounts = (accounts) => (accounts.length > 0
-    ? <View>
-        {accounts.map(account => (<Account account={account}/>))}
-      </View>
-    : <View>
-      <Text>Swipe Left to Add an Ethereum Account</Text>
-    </View>)
+	renderAccounts = (accounts) =>
+		accounts.length > 0 ? (
+			<View>
+				<AddressDetailed account={accounts[0]} />
+			</View>
+		) : (
+			<View>
+				<Text>Swipe Left to Add an Ethereum Account</Text>
+			</View>
+		);
 
-  render() {
-    const {cameraActive} = this.state
-    const {accounts} = this.props
-    return (
-      <SafeAreaView style={styles.appContainer}>
-        <Swiper
-          index={1}
-          loop={false}
-          showsPagination={false}
-          ref={swiper => this.swiper = swiper}
-          onIndexChanged={this.onIndexChanged}>
-          {[
-            (<Scanner active={cameraActive} onBarCodeRead={this._qrCodeScanned}/>), this.renderAccounts(accounts)
-          ]}
-        </Swiper>
-      </SafeAreaView>
-    )
-  }
+	render() {
+		const { cameraActive } = this.state;
+		const { accounts } = this.props;
+		return (
+			<SafeAreaView style={styles.appContainer}>
+				<Swiper
+					index={1}
+					loop={false}
+					showsPagination={false}
+					ref={(swiper) => (this.swiper = swiper)}
+					onIndexChanged={this.onIndexChanged}
+				>
+					{[
+						<Scanner active={cameraActive} onBarCodeRead={this._qrCodeScanned} />,
+						this.renderAccounts(accounts)
+					]}
+				</Swiper>
+			</SafeAreaView>
+		);
+	}
 }
 
-const mapStateToProps = (state) => ({accounts: state.accounts})
-export default connect(mapStateToProps, {addAndPopulateAccount})(App)
+const mapStateToProps = (state) => ({
+	accounts: state.accounts
+});
+export default connect(mapStateToProps, {
+	addAndPopulateAccount
+})(App);
 
 const styles = StyleSheet.create({
-  appContainer: {
-    flex: 1,
-    backgroundColor: '#EEEEEE'
-  },
-  container: {
-    flex: 1
-  },
-  textInput: {
-    flex: 2
-  },
-  button: {
-    flex: 1
-  }
-})
+	appContainer: {
+		flex: 1,
+		backgroundColor: '#F2F2F2'
+	},
+	container: {
+		flex: 1
+	},
+	textInput: {
+		flex: 2
+	},
+	button: {
+		flex: 1
+	}
+});
---
+++ src/components/AddressDetailed/index.js
@@ -0,0 +1,78 @@
+import React, { Component } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import { Icon, Card, Screen, Title, Caption, Heading, Subtitle, Row, Spinner, ListView, Divider } from '@shoutem/ui';
+import { utils } from 'ethers';
+
+export const AddressDetailed = ({ account }) => {
+	const { address, balances, transactions } = account;
+
+	const renderTransaction = (transaction) => (
+		<View>
+			<Row>
+				<Icon name={address === transaction.to ? 'left-arrow' : 'right-arrow'} />
+				<View style={{ display: 'flex', flexDirection: 'column' }}>
+					<Subtitle>{`${utils.etherSymbol} ${transaction.value}`}</Subtitle>
+					<Text>{new Date(transaction.timeStamp * 1000).toLocaleString()}</Text>
+				</View>
+			</Row>
+			<Divider styleName="line" />
+		</View>
+	);
+
+	return (
+		<View style={styles.container}>
+			<View style={styles.top}>
+				<View style={styles.infoCard}>
+					{balances ? (
+						<Heading styleName="h-center">{`${utils.etherSymbol} ${balances.eth}`}</Heading>
+					) : (
+						<Spinner />
+					)}
+					<Caption styleName="h-center">{address}</Caption>
+				</View>
+			</View>
+			<View style={styles.bottom}>
+				<Title>Transactions</Title>
+				<ListView
+					loading={transactions === undefined}
+					data={transactions || []}
+					renderRow={renderTransaction}
+				/>
+			</View>
+		</View>
+	);
+};
+
+const styles = StyleSheet.create({
+	container: {
+		height: '100%',
+		width: '100%',
+		display: 'flex',
+		flexDirection: 'column'
+	},
+	top: {
+		flex: 1,
+		width: '100%',
+		justifyContent: 'center',
+		alignItems: 'center'
+	},
+	infoCard: {
+		display: 'flex',
+		flexDirection: 'column',
+		alignItems: 'center',
+		justifyContent: 'center',
+		width: '90%',
+		height: '50%',
+		backgroundColor: '#ffffff'
+	},
+	bottom: {
+		flex: 2,
+		height: '100%'
+	},
+	inText: {
+		color: '#AFDB8C'
+	},
+	outText: {
+		color: '#D88594'
+	}
+});
